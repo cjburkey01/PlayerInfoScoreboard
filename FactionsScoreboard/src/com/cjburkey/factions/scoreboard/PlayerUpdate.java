a57c9db2bdb0d454bb3cbf939fb238abf33bd4fe
cjburkey01 <cjburkey01@gmail.com>
2016-02-24 20:32:30 -0500

    v1.0.0

--- FactionsScoreboard/src/com/cjburkey/factions/scoreboard/PlayerUpdate.java
+++ FactionsScoreboard/src/com/cjburkey/factions/scoreboard/PlayerUpdate.java
@@ -9,14 +9,16 @@ public class PlayerUpdate {
 	public static final void doit(Player p) {
 		MPlayer player = MPlayer.get(p);
 		Faction f = player.getFaction();
-
-		FS.addScore(FS.getConfigString("LineBreak") + " ", p);
-		FS.addScore(FS.getConfigString("HealthText") + p.getHealth(), p);
-		FS.addScore(FS.getConfigString("FoodText") + p.getFoodLevel(), p);
-		FS.addScore(FS.getConfigString("MoneyText") + FS.getEcon().format(FS.getEcon().getBalance(p)), p);
-		FS.addScore(FS.getConfigString("FactionText") + f.getName(), p);
-		FS.addScore(FS.getConfigString("SiteText"), p);
-		FS.addScore(FS.getConfigString("LineBreak"), p);
+		
+		if(!p.hasPermission("fs.disable")) {
+			FS.addScore(FS.getConfigString("LineBreak") + " ", p);
+			FS.addScore(FS.getConfigString("HealthText") + p.getHealth(), p);
+			FS.addScore(FS.getConfigString("FoodText") + p.getFoodLevel(), p);
+			FS.addScore(FS.getConfigString("MoneyText") + FS.getEcon().format(FS.getEcon().getBalance(p)), p);
+			FS.addScore(FS.getConfigString("FactionText") + f.getName(), p);
+			FS.addScore(FS.getConfigString("SiteText"), p);
+			FS.addScore(FS.getConfigString("LineBreak"), p);
+		}
 		
 		if(FS.getPlugin().getConfig().getBoolean("DebugSpam")) Util.log("&2Updated Display");
 		
